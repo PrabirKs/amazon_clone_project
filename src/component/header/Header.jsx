50aa4c149af98c22f9f7155547c4162a40233439
PrabirKs <pkxsahoo@gmail.com>
2023-12-18 18:56:27 +0530

    taildwind added and configured with colors,screen width and fonts

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="w-full bg-amazon_blue text-white px-4 py-3 ">
+      {/* ============ Image Start here ================ */}
+      {/* ============ Image End here ======  */}
+      {/* ============ Deliver Start here ============== */}
+      {/* ============ Deliver End here ================ */}
+      {/*============ Search Start here =============== */}
+      {/* ====== ==== Search End here ============ */}
+      {/* ======== Signin Start here =============== */}
+      {/* ============ Signin End here ================= */}
+      {/* ===== ==== Orders Start here =============== */}
+      {/* ============ Orders End here ================= */}
+      {/* ============ Cart Start here ================= */}
+      {/* ============ Cart End here =================== */}
+    </div>
+  );
+}
+
+export default Header;
